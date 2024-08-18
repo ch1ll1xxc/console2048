45607069a6e6b01d5125faed5c32e744ca9fae60
ch1ll1xxc <olegebronze@gmail.com>
2024-08-18 14:33:57 +0300

    Add write_nums func

--- main.py
+++ main.py
@@ -26,3 +26,12 @@ def list_rot90(data, times=1):
                     for i in range (n)]
         data = rot_data
     return rot_data
+
+def write_nums():
+    global deck
+    deck = start_deck[:]
+    for row in range (4):
+        for col in range (4):
+            if nums[row][col]:
+                for i in range(len(str(nums[row][col]))):
+                    deck[22 + (row * (22 * 2)) + 1 + col * 5 + i] = str(nums[row][col])[i]
